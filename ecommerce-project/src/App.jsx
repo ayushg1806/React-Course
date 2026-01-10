676f4ad5f299c70470bd7783b48c449c201e9e27
ayushg1806 <agupta9617@gmail.com>
2026-01-10 09:13:54 +0530

    Separate the Checkout Header component

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { HomePage } from './pages/HomePage.jsx'
-import { CheckoutPage } from './pages/CheckoutPage.jsx'
+import { CheckoutPage } from './pages/checkout/CheckoutPage.jsx'
 import { OrdersPage } from './pages/OrdersPage.jsx'
 import { TrackingPage } from './pages/TrackingPage.jsx'
 import { Routes, Route } from 'react-router'
