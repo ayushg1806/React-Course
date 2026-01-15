13482a736d578d14cdf5c23ed098e322b28d4260
ayushg1806 <agupta9617@gmail.com>
2026-01-15 10:45:41 +0530

    Separate the orders page components

---
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect, useState, Fragment } from "react";
+import { Header } from "../../components/Header";
+import { OrdersGrid } from "./OrdersGrid";
+import "./OrdersPage.css";
+
+export function OrdersPage({ cart }) {
+  useEffect(() => {
+    document.title = "Orders";
+
+    const favicon = document.querySelector("link[rel='icon']");
+    if (favicon) {
+      favicon.href = "/images/orders-favicon.png";
+    }
+  }, []);
+
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/orders?expand=products").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <title>Orders</title>
+      <Header cart={cart} />
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
+        <OrdersGrid orders={orders} />
+      </div>
+    </>
+  );
+}
