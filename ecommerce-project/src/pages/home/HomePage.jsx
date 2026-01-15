ec7d3c2391325b70673f162fb7935dd8b19ad6da
ayushg1806 <agupta9617@gmail.com>
2026-01-15 10:23:15 +0530

    Separate the home page components

---
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Header } from '../../components/Header.jsx';
+import { ProductsGrid } from './ProductsGrid.jsx';
+import './HomePage.css';
+
+export function HomePage({ cart }) {
+  useEffect(() => {
+    document.title = "Ecommerce Project";
+
+    const favicon = document.querySelector("link[rel='icon']");
+    if (favicon) {
+      favicon.href = "/images/home-favicon.png";
+    }
+  }, []);
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/products')
+      .then((response) => {
+        setProducts(response.data);
+    }, []);
+  })
+
+  return (
+    <>
+      <title>Ecommerce Project</title>
+      <link rel="icon" type="image/svg+xml" href="/images/home-favicon.png" />
+      <Header cart={cart} />
+
+      <div className="home-page">
+        <ProductsGrid products={products} />
+      </div>
+    </>
+  );
+}
